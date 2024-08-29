4590393fcd5bff959c425b4445232e13d2344acb
oznur.demir <oznur.demir@beymen.com>
2024-08-29 21:47:02 +0300

    Completed home_page_cubit.dart

--- lib/ui/cubit/home_page_cubit.dart
+++ lib/ui/cubit/home_page_cubit.dart
@@ -1,18 +1,15 @@
+import 'package:bloc_pattern_sample_project/data/repo/math_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePageCubit extends Cubit<int>{
   HomePageCubit() : super(0); // Bu class çalıştığında varsayılan değer 0 olacak.
-
+  var mRepo = MathRepository();
   void sum(String num1, String num2){
-    int number1 = int.parse(num1);
-    int number2 = int.parse(num2);
-    int sum = number1 + number2;
-    emit(sum); // Tetikleme ve veri gönderme
+    int result = mRepo.sum(num1, num2);
+    emit(result); // Tetikleme ve veri gönderme
   }
   void multiplex(String num1, String num2){
-    int number1 = int.parse(num1);
-    int number2 = int.parse(num2);
-    int multiplex = number1 * number2;
-    emit(multiplex); // Tetikleme ve veri gönderme
+    int result = mRepo.multiplex(num1, num2);
+    emit(result); // Tetikleme ve veri gönderme
   }
 }
